80d082a8f5e72858d49061ec8cc97fb451fff11a
Aida Mobli <aidaw.mobli@gmail.com>
2024-07-14 19:21:01 +0330

    fix the data manager problem

--- app/data_manager.py
+++ app/data_manager.py
@@ -3,33 +3,31 @@ import sqlite3
 from database_manager import DB
 
 
-
-def on_message(client, userdata, msg):
-    print(f"Received message: {msg.payload.decode()} on topic {msg.topic}")
-    temp , humi =  msg.payload.decode().split(" ")
-    db.new_record(msg.topic,float(temp),float(humi))
-
-def on_connect(client, userdata, flags, rc,properties):
-    if rc == 0:
-        print("Connected successfully")
-        client.subscribe("sensor/data/#")
-    else:
-        print(f"Connect failed with code {rc}")
-
-
-def get_data_from_hubs():
-
-    db = DB()
+class Reciever:
+    def __init__(self) -> None:
+        self.db = DB()
     
-    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+        client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
-    client.on_connect = on_connect
-    client.on_message = on_message
+        client.on_connect = self.on_connect
+        client.on_message = self.on_message
 
-    client.connect("localhost", 1883, 60)
+        client.connect("localhost", 1883, 60)
 
-    client.loop_forever()
+        client.loop_forever()
 
+    def on_message(self,client, userdata, msg):
+        print(f"Received message: {msg.payload.decode()} on topic {msg.topic}")
+        temp , humi =  msg.payload.decode().split(" ")
+        self.db.new_record(msg.topic,float(temp),float(humi))
+
+    def on_connect(self,client, userdata, flags, rc,properties):
+        if rc == 0:
+            print("Connected successfully")
+            client.subscribe("sensor/data/#")
+        else:
+            print(f"Connect failed with code {rc}")
+    
 
 if __name__ == "__main__":
-    get_data_from_hubs()
+    Reciever()
