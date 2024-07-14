05f05f8c308b446c98f9463541f5e25648e50e7d
Aida Mobli <aidaw.mobli@gmail.com>
2024-07-14 23:56:20 +0330

    get data from api and show them on screen

---
+++ dashboard/src/App.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState} from 'react';
+import './App.css';
+import axios from 'axios';
+
+export type Hub = {
+  hub_id: string
+};
+
+function App() {
+  const [hubs, setHubs] = useState<Hub[] | null>(null);
+  useEffect(() => {
+    const url = "/hub_list"
+    axios.get(url).then((response) => {setHubs(response.data);});
+  }, []);
+  return <div className="App">
+      {
+        hubs ? 
+        hubs.map((hub) => {
+          return <p>{hub.hub_id}</p>
+        }) : null
+      }</div>;
+
+}
+
+export default App;
